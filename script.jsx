d45990056366a6ac8b04e785ec1f5b7f49e2bf66
allankiplangat <allankiplangat22@outlook.com>
2018-02-14 10:01:41 +0300

    Reusable components

--- script.jsx
+++ script.jsx
@@ -1,22 +1,48 @@
 var Button = React.createClass({
+    localHandleClick: function() {
+        this.props.localHandleClick(this.props.increment);
+    },
+
+    render: function(){
+        return (
+            <button onClick={this.localHandleClick}>{this.props.increment}</button>
+        )
+    }
+});
+
+var Result = React.createClass({
+    render: function(){
+        return( 
+            <div>{this.props.localCounter}</div>
+        )
+    }
+});
+
+var Main = React.createClass({
     getInitialState: function() {
         return {
             counter: 0,
         };
     },
 
-    handleClick: function() {
+    handleClick: function(increment) {
         this.setState({
-            counter: this.state.counter+1,
-        })
+            counter: this.state.counter+increment,
+        });
 
     },
 
     render: function(){
-        return (
-            <button onClick={this.handleClick}>{this.state.counter}</button>
+        return(
+            <div>
+                <Button localHandleClick={this.handleClick} increment={1} />
+                <Button localHandleClick={this.handleClick} increment={5} />
+                <Button localHandleClick={this.handleClick} increment={10} />
+                <Button localHandleClick={this.handleClick} increment={100} />
+                <Result localCounter={this.state.counter}/>
+            </div>
         )
     }
-});
+})
 
-React.render(<Button />, document.getElementById("root"));
+React.render(<Main />, document.getElementById("root"));
